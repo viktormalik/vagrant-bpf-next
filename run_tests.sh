e042330ccca93e57439471f2393cb2a26f0f1f8f
Viktor Malik <viktor.malik@gmail.com>
2022-10-27 15:51:59 +0200

    run_tests.sh: print summary at the end
    The summary contains the last line of each executed test.

--- run_tests.sh
+++ run_tests.sh
@@ -9,15 +9,18 @@
 
 TESTDIR=${TESTDIR:=/bpf-next/tools/testing/selftests/bpf}
 ALL_TESTS="test_progs test_progs-no_alu32 test_maps test_verifier"
+LOG=""
 
 run_test() {
     testname=$1
     if [ -f /vagrant/$testname-denylist ]; then
         denylist=$(cat /vagrant/$testname-denylist | cut -d' ' -f1 | tr '\n' ',')
-        $TESTDIR/$testname -d $denylist
+        cmd="$TESTDIR/$testname -d $denylist"
     else
-        $TESTDIR/$testname
+        cmd="$TESTDIR/$testname"
     fi
+    LOG="$LOG$testname: $($cmd | tee /dev/tty | tail -n 1)\n"
+    echo
 }
 
 if [ $# -eq 0 ]; then
@@ -30,4 +33,15 @@ rc=0
 for t in $tests; do
     run_test $t || rc=1
 done
+
+echo "Summary"
+echo "-------"
+echo -e $LOG
+
+if [ $rc -eq 0 ]; then
+    echo "All tests have passed"
+else
+    echo "Some tests have failed"
+fi
+
 exit $rc
